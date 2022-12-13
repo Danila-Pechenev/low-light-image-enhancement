2bbe0091f0fdc443a98f65b840d4388e59ff2d69
Danila-Pechenev <danil-pechenev@mail.ru>
2022-12-14 00:17:39 +0300

    Cosmetics

--- app/model.py
+++ app/model.py
@@ -15,19 +15,19 @@ def run_model(image_bytes: io.BytesIO, model: keras.Model) -> Image.Image:
     height: int
     width, height = image.size
     image: Image.Image = image.resize((960, 640))
-    image_array: np.ndarray = keras.utils.img_to_array(image)
-    image_array: np.ndarray = image_array.astype("float32") / 255.0
-    image_array: np.ndarray = np.expand_dims(image_array, axis=0)
-    output: np.ndarray = model.predict(image_array)
-    output_image_array: np.ndarray = output[0] * 255.0
-    output_image_array: np.ndarray = output_image_array.clip(0, 255)
-    output_image_array: np.ndarray = output_image_array.reshape(
-        (np.shape(output_image_array)[0], np.shape(output_image_array)[1], 3)
+    image_array1: np.ndarray = keras.utils.img_to_array(image)
+    image_array2: np.ndarray = image_array1.astype("float32") / 255.0
+    image_array3: np.ndarray = np.expand_dims(image_array2, axis=0)
+    output: np.ndarray = model.predict(image_array3)
+    output_image_array1: np.ndarray = output[0] * 255.0
+    output_image_array2: np.ndarray = output_image_array1.clip(0, 255)
+    output_image_array3: np.ndarray = output_image_array2.reshape(
+        (np.shape(output_image_array2)[0], np.shape(output_image_array2)[1], 3)
     )
-    output_image_array: np.ndarray = np.uint32(output_image_array)
-    output_image_array: np.ndarray = output_image_array.astype(np.uint8)
-    output_image: Image.Image = Image.fromarray(output_image_array)
-    output_image: Image.Image = output_image.resize((width, height))
+    output_image_array4: np.ndarray = np.uint32(output_image_array3)
+    output_image_array5: np.ndarray = output_image_array4.astype(np.uint8)
+    output_image1: Image.Image = Image.fromarray(output_image_array5)
+    output_image2: Image.Image = output_image1.resize((width, height))
     # Uncomment if necessary:
     # output_image.save("user_data/output.jpg")
-    return output_image
+    return output_image2
